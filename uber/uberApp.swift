1386600b71c9a71f05fe04027200896f0bbbdc5b
Elizabeth Kang <liz.kang@hey.com>
2022-11-21 10:49:51 -0600

    Initial Commit

---
+++ uber/uberApp.swift
@@ -0,0 +1,17 @@
+//
+//  uberApp.swift
+//  uber
+//
+//  Created by Elizabeth Kang on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct uberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
